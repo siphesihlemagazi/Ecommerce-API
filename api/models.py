b28d6668ba90e6db187d7028c4e40d0ac47a2c95
siphesihlemagazi <siphemagazi@outlook.com>
2022-06-27 19:31:54 +0200

    added models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
+from django_resized import ResizedImageField
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=250)
+
+    class Meta:
+        ordering = ['-name']
+
+    def __str__(self):
+        return f"{self.name}".title()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    price = models.FloatField()
+    description = models.CharField(max_length=250)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = ResizedImageField(default='product.jpeg', size=[300, 300],
+                              crop=['middle', 'center'], quality=75,
+                              force_format="PNG",
+                              upload_to='products', blank=True, null=True)
+
+    class Meta:
+        ordering = ['-name']
+
+    def __str__(self):
+        return f"{self.name} ({self.category}) (R{self.price})".title()
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return f"{self.name} {self.date_created}".title()
