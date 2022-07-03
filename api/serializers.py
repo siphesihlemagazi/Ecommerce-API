cbf782e75783d682b0f027c2ff492fe3ae431cb8
siphesihlemagazi <siphemagazi@outlook.com>
2022-07-03 18:44:36 +0200

    added custom fields to order serializer

--- api/serializers.py
+++ api/serializers.py
@@ -19,14 +19,19 @@ class ProductSerializer(serializers.ModelSerializer):
 class OrderSerializer(serializers.ModelSerializer):
 
     product_name = serializers.SerializerMethodField()
+    description = serializers.SerializerMethodField()
 
     def get_product_name(self, obj):
         return obj.product.name
 
+    def get_description(self, obj):
+        return obj.product.description
+
     class Meta:
         model = Order
         fields = "__all__"
-        read_only_fields = ['status', 'date_created', 'total_price', 'product_name']
+        read_only_fields = ['status', 'date_created', 'total_price',
+                            'product_name', 'description']
 
 
 class UserSerializer(serializers.ModelSerializer):
